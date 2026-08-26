8f61f1fdd5aadb9080cf6a7500a6d54b96b66fb1
devjocas <joaquimmenianga99@gmail.com>
2026-08-26 16:12:39 +0530

    callback queue e microtask

---
+++ macroStack/stack.js
@@ -0,0 +1,48 @@
+// // CALLBACK QUEUE(MACROTASK) E MICROTASK
+console.log("Primeiro")
+setTimeout(()=>{
+    console.log("Segundo")
+},0)
+console.log("terceiro")
+Promise.resolve().then(()=>{
+    console.log("Quarto")
+})
+
+// queueMicrotask e Promise.reslove().then()
+queueMicrotask(()=>console.log("A"))
+Promise.resolve().then(()=>console.log("B"))
+queueMicrotask(()=>console.log("C"))
+Promise.resolve().then(()=>console.log('D'))
+
+console.log('1');
+
+setTimeout(() => {
+  console.log('timeout');
+}, 0);
+
+Promise.resolve().then(() => console.log('promise'));
+
+queueMicrotask(() => console.log('queueMicrotask'));
+
+console.log('2');
+
+//Promise Resolution Order
+
+console.log('script start');
+
+Promise.resolve()
+  .then(() => console.log('promise 1'))
+  .then(() => console.log('promise 1 chain'));
+
+Promise.resolve()
+  .then(() => console.log('promise 2'));
+
+setTimeout(() => console.log('timeout'), 0);
+
+console.log('script end');
+
+Promise.resolve()
+  .then(() => console.log('then 1'))
+  .catch(() => console.log('catch'))
+  .finally(() => console.log('finally'))
+  .then(() => console.log('then 2'));
